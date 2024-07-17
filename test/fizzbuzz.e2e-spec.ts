c83f5d73de2bbac0feeec88bda65e2295869c451
agrotraze <infoagrotraze@gmail.com>
2024-07-17 12:49:41 -0500

    first commit

---
+++ test/fizzbuzz.e2e-spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('FizzBuzz(e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+it('/fizzbuzz/3 (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/fizzbuzz/3')
+      .expect(200)
+      .expect('Fizz');
+  });
+});
